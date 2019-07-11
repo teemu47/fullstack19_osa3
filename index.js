4cd2059352ae3d1ff1d20f02a25c66b5945973ae
Teemu Ijäs <ijasteemu@gmail.com>
2019-07-11 17:03:41 +0300

    3.2

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  res.end(`Phonebook has info for ${persons.length} people \n\n${new Date()}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}`)
