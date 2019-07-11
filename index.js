916b73eb1b3639ede6fb2bf5d355b15dbe77e858
Teemu Ijäs <ijasteemu@gmail.com>
2019-07-11 17:16:52 +0300

    3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+var persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -35,6 +35,11 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  persons = persons.filter(p => p.id !== Number(req.params.id))
+  res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
