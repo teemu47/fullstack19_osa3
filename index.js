c3e07d75f9172a0db05618725b3cb59e83b99d83
Teemu Ijäs <ijasteemu@gmail.com>
2019-07-17 20:01:35 +0300

    3.16

--- index.js
+++ index.js
@@ -18,44 +18,27 @@ morgan.token('post-body', function (req, res) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
-var persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "040-123456",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "040-123456",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "040-123456",
-    // id: 4
-  }
-]
-
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({})
     .then(result => {
       res.json(result)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
-      res.json(person.toJSON())
+      if (person) {
+        res.json(person.toJSON());
+      } else {
+        res.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const reqName = req.body.name
   const reqNumber = req.body.number
   if (!reqName || !reqNumber) {
@@ -71,18 +54,30 @@ app.post('/api/persons', (req, res) => {
     .then(response => {
       res.json(response.toJSON())
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-  res.end(`Phonebook has info for ${persons.length} people \n\n${new Date()}`)
-})
+// app.get('/info', (req, res) => {
+//   res.end(`Phonebook has info for ${persons.length} people \n\n${new Date()}`)
+// })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error)
+  
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () =>{
